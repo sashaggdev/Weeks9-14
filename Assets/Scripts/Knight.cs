ccd27477da56b9dd879e102af49d6df2c0b159b5
sashaggdev <arcanasasha@gmail.com>
2026-03-25 09:41:42 -0400

    Added sound effects and animation events

---
+++ Assets/Scripts/Knight.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+
+    public AudioSource SFX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Footstep()
+    {
+        //Debug.Log("Footstep");
+        SFX.Play();
+    }
+}
